c2bf1ab847f59d0eea26ca08bd35fc1785583e12
winnie daisy <winniedaisyaber@gmail.com>
2025-11-11 22:02:44 +0300

    pulling changes

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,21 @@ export default defineConfig({
         target: 'http://142.93.94.236:8000',
         changeOrigin: true,
         secure: false,
+        configure: (proxy, options) => {
+          proxy.on('error', (err, req, res) => {
+            console.log('proxy error', err);
+            res.writeHead(500, {
+              'Content-Type': 'text/plain',
+            });
+            res.end('Something went wrong with the proxy.');
+          });
+          proxy.on('proxyReq', (proxyReq, req, res) => {
+            console.log('Sending Request to the Target:', req.method, req.url);
+          });
+          proxy.on('proxyRes', (proxyRes, req, res) => {
+            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          });
+        },
       }
     }
   }
